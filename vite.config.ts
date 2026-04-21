7b8a64cd84d7fcd1855033d3bb8835244845dceb
Nicolas Benitez <nicopbenitez84@gmail.com>
2026-04-20 23:30:04 -0300

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/Futbol-y-Porro-FC/" : "/",
   server: {
     host: "::",
     port: 5173,
